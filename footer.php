dc263d9aef81890b02ba9ad7f03272d5bd5654e8
laozhu <devzsj@163.com>
2024-04-26 18:49:09 +0800

    优化底部样式

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<footer id="footer" class="position-absolute bottom-0 w-100 text-secondary text-center">
+<footer id="footer" class="position-absolute bottom-0 w-100 text-secondary fs-6 text-center">
     <p>
         &copy; <?php echo date('Y'); ?> All rights reserved.
         <?php if ($this->options->beian) : ?>
-            <a class="text-decoration-none" href="http://beian.miit.gov.cn" target="_blank"><?php $this->options->beian() ?></a>
+            <a class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="http://beian.miit.gov.cn" target="_blank"><?php $this->options->beian() ?></a>
         <?php endif; ?>
-        Powered by <a class="text-decoration-none" href="http://typecho.org" target="_blank">Typecho</a>.
-        Theme by <a class="text-decoration-none" href="https://ilaozhu.com" target="_blank">BeaconNav</a>.
+        Powered by <a class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="http://typecho.org" target="_blank">Typecho</a>.
+        Theme by <a class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="https://ilaozhu.com" target="_blank">BeaconNav</a>.
     </p>
 </footer><!-- end #footer -->
 </div>
