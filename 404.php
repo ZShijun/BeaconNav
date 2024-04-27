8b52f88d6565ba0b9f1c679a6ed095d903aa6114
laozhu <devzsj@163.com>
2024-04-27 16:46:57 +0800

    优化404页面

--- 404.php
+++ 404.php
@@ -1,21 +1,16 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<!DOCTYPE html>
-<html>
-<?php $this->need('head.php'); ?>
 
-<body>
-    <div id="loader"></div>
-    <div id="content">
-        <?php $this->need('header.php'); ?>
-        <section>
-            <h2>404 - <?php _e('页面没找到'); ?></h2>
-            <p><?php _e('抱歉，没有找到你要的内容...'); ?></p>
+<?php $this->need('header.php'); ?>
+<section>
+    <div class="container">
+        <div class="text-center mt-3 py-5 bg-light shadow rounded">
+            <h2 class="text-danger" style="font-size: 8rem; letter-spacing: 1rem">404</h2>
+            <p class="lead"><?php _e('抱歉，没有找到你要的内容...'); ?></p>
             <div>
+                <a href="javascript:history.back()" class="btn btn-outline-secondary me-5"><?php _e('返回上一页'); ?></a>
                 <a class="btn btn-danger" href="/"><?php _e('返回首页'); ?></a>
             </div>
-        </section>
-        <?php $this->need('footer.php'); ?>
+        </div>
     </div>
-</body>
-
-</html>
+</section>
+<?php $this->need('footer.php'); ?>
