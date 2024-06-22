778ff7600ba8e305b0d0bc56a956c46ad1d8a077
laozhu <devzsj@163.com>
2024-06-22 12:16:25 +0800

    调整透明度

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <aside id="sidebar">
-    <div class="recently-used bg-black bg-opacity-75 shadow rounded-end">
+    <div class="recently-used bg-black bg-opacity-50 shadow rounded-end">
         <span class="title text-white bg-black bg-opacity-75 shadow rounded-end">
             <?php _e('最近') ?>
         </span>
@@ -8,7 +8,7 @@
             <p class="text-white flex-fill mt-5 text-center"><?php _e('暂无数据...') ?></p>
         </div>
     </div>
-    <div class="favorite-used bg-black bg-opacity-75 shadow rounded-end">
+    <div class="favorite-used bg-black bg-opacity-50 shadow rounded-end">
         <span class="title text-white bg-black bg-opacity-75 shadow rounded-end">
             <?php _e('收藏') ?>
         </span>
